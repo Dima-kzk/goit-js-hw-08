f9c90f5f298b7372998d54e35402734f7a2c5411
Dima-kzk <dimakzk@yahoo.com>
2023-04-05 18:01:38 +0300

    correction 2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,14 +3,20 @@ import throttle from 'lodash.throttle';
 const FEEDBACK_STATE = 'feedback-form-state';
 
 const feedbackForm = document.querySelector('.feedback-form');
-const data = {};
+let data = {};
 
 feedbackForm.addEventListener(
   'input',
   throttle(event => {
     event.preventDefault();
-    console.log('input');
+
+    if (event.target.name === feedbackForm[0].name) {
+      data[feedbackForm[1].name] = feedbackForm[1].value;
+    } else if (event.target.name === feedbackForm[1].name) {
+      data[feedbackForm[0].name] = feedbackForm[0].value;
+    }
     data[event.target.name] = event.target.value;
+
     localStorage.setItem(FEEDBACK_STATE, JSON.stringify(data));
   }, 500)
 );
@@ -23,13 +29,23 @@ window.addEventListener('DOMContentLoaded', () => {
     return;
 
   const temp = JSON.parse(localStorage.getItem(FEEDBACK_STATE));
-
-  feedbackForm.email.value = temp.email;
-  feedbackForm.message.value = temp.message;
+  feedbackForm.email.value = temp.email === undefined ? '' : temp.email;
+  feedbackForm.message.value = temp.message === undefined ? '' : temp.message;
 });
 
 feedbackForm.addEventListener('submit', event => {
   event.preventDefault();
+
+  if (feedbackForm.email.value === '') {
+    alert('Введіть e-mail');
+    return;
+  }
+
+  if (feedbackForm.message.value === '') {
+    alert('Введіть повшдомленя');
+    return;
+  }
+
   feedbackForm.email.value = '';
   feedbackForm.message.value = '';
   data = {};
@@ -39,7 +55,7 @@ feedbackForm.addEventListener('submit', event => {
   )
     return;
   const temp = JSON.parse(localStorage.getItem(FEEDBACK_STATE));
+
   console.log(temp);
-  console.log('data');
   localStorage.removeItem(FEEDBACK_STATE);
 });
